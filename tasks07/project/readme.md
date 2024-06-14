539c22216733f818c0b26751a719d52ba5738750
Jean Fernandes <jean.fernandes.eng@gmail.com>
2024-06-14 13:11:14 -0300

    Update Main Function

---
+++ tasks07/project/readme.md
@@ -0,0 +1,10 @@
+# Links 
+
+## Canal do Github da placa
+
+- https://github.com/vcc-gnd/YD-ESP32-S3
+
+## Canal do Projeto
+
+- https://github.com/f3rn4ndes/freertos-esp32/tree/main/tasks07
+
--- tasks07/src/main.cpp
+++ tasks07/src/main.cpp
@@ -7,13 +7,21 @@ void setup()
   delay(kInitialDelay);
   Serial.begin(kSerialBaudRate);
   Serial.setDebugOutput(kAppLogMessages);
-  ESP_LOGI(kMainLogTag, "Starting Application....");
+  ESP_LOGV(kMainLogTag, "Starting Application....");
+  Serial.println("(P) Starting Application....");
 }
 
 void loop()
 {
+  static int counter = 5;
   // put your main code here, to run repeatedly:
-
-  ESP_LOGI(kMainLogTag, "Testing ESP32-S3-WROOM-1....");
+  ESP_LOGW(kMainLogTag, "Testing ESP32-S3-WROOM-1....");
+  Serial.println("(P) Testing ESP32-S3-WROOM-1....");
   delay(kPrintDelayMs);
+  if (counter)
+  {
+    counter--;
+    if (!counter)
+      Serial.setDebugOutput(false);
+  }
 }
